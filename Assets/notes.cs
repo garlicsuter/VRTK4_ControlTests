fa9c4449fceecd14527ac2fe3a70812778674147
garlicsuter <markasuter@gmail.com>
2020-01-09 15:38:59 -0500

    initial commit
    changed Tracked Alias location to beside buttons to make new buttons.

---
+++ Assets/notes.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class notes : MonoBehaviour
+{
+    // Changed "Tracked Alias" position to change starting position
+    //
+}
