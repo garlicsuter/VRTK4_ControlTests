38beeb48c8567e32c7b2e15d53f76870bac4b6d6
garlicsuter <markasuter@gmail.com>
2020-02-12 19:53:23 -0500

    testing scoreboard - no work.

---
+++ Assets/SuterAssets/Scripts/SnitchBall.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SnitchBall : MonoBehaviour
+{
+    public int Score = 0;
+    public TMP_Text ScoreText;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //if (collision.collider.tag == "snitch")
+        //{
+        //    Debug.Log("Gooooooaaaaaal!");
+        //    Score += 1;
+        //    UpdateScore();
+        //}
+        if (collision.gameObject.CompareTag("snitch")){
+
+            Debug.Log("Goal?");
+
+        }
+    }
+
+
+    void UpdateScore()
+    {
+        ScoreText.SetText(Score.ToString());
+    }
+}
